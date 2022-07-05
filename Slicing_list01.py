eec09a5994a2131c526333344b738d43c8a36d38
Diyorbek <baxshullayevd4@gmail.com>
2022-07-05 15:17:02 +0500

    1

--- Slicing_list01.py
+++ Slicing_list01.py
@@ -1,6 +1,3 @@
-import numbers
-
-
 def main(numbers):
     """
     A list called numbers is given. Return the items in the odd index.
@@ -9,7 +6,7 @@ def main(numbers):
     Returns:
         list: return answer.
     """
-    numbers=[0,1,2,3,4,5]
+    
     return numbers[::2]
-
-print(main(numbers))
+a = [0,1,2,3,4,5]
+print(main(a))
